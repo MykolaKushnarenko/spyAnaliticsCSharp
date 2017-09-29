2594b944519b183285c56ea081be6df4c462f9aa
NikoKushnarenko <nikokusha1997@mail.ru>
2017-09-29 18:17:35 +0300

    first commit

---
+++ AppWork/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+
+namespace AppWork
+{
+    class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static string En = "~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./↑←↓ ";
+        private static string Ru = "Ё!\"№;%:?*()_+ЙЦУКЕНГШЩЗХЪ/ФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,ё1234567890-=йцукенгшщзхъ\\фывапролджэячсмитьбю.↑←↓ ";
+
+        private static char[] enen = En.ToCharArray();
+        private static char[] ruru = Ru.ToCharArray();
+
+        private static Dictionary<char, char> dictionary = new Dictionary<char, char>();
+        private static void Language_mas()
+        {
+            for (int i = 0; i < enen.Length; i++)
+            {
+                dictionary.Add(enen[i], ruru[i]);
+            }
+        }
+
+        private static void Litera(string ma)
+        {
+            InputLanguage myCurrent = InputLanguage.CurrentInputLanguage;
+            char[] a = ma.ToCharArray();
+            if (ma != "LShiftKey")
+            {
+                if (myCurrent.Culture.Name == "ru-RU")
+                {
+                    Console.WriteLine(dictionary[a[0]]);
+                }
+                if (myCurrent.Culture.Name == "ua-UA")
+                {
+                    Console.WriteLine(dictionary[a[0]]);
+                }
+                else Console.WriteLine(ma);
+                //Console.WriteLine(ma[0]);
+
+            }
+            else
+                Console.WriteLine(ma);
+
+
+        }
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+
+                int keyCode = Marshal.ReadInt32(lParam);
+                KeysConverter kc = new KeysConverter();
+                string a = InputLanguage.CurrentInputLanguage.Culture.DisplayName;
+                string mystring = kc.ConvertToString((Keys)keyCode);
+                Litera(mystring);
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        static void Main(string[] args)
+        {
+            Language_mas();
+            _hookID = SetHook(_proc);
+            Application.Run();
+            UnhookWindowsHookEx(_hookID);
+        }
+    }
+}
---
+++ KeyboardHooks/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static KeyboardHooks.CoreFunctionImport;
+
+namespace KeyboardHooks
+{
+    public partial class Form1 : Form
+    {
+
+        public Dictionary<char, char> dictionary = new Dictionary<char, char>();
+        public Form1()
+        {
+            InitializeComponent();
+            string En = "~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./↑←↓ ";
+            string Ru = "Ё!\"№;%:?*()_+ЙЦУКЕНГШЩЗХЪ/ФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,ё1234567890-=йцукенгшщзхъ\\фывапролджэячсмитьбю.↑←↓ ";
+
+            char[] enen = En.ToCharArray();
+            char[] ruru = Ru.ToCharArray();
+            for (int i = 0; i < enen.Length; i++)
+            {
+                dictionary.Add(enen[i], ruru[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CoreFunctionImport._hookID = SetHook(CoreFunctionImport.HookCallback);
+            CoreFunctionImport.CallbackAction = Display;
+        }
+
+        public void Display(string s)
+        {
+            textBox2.Text = InputLanguage.CurrentInputLanguage.Culture.DisplayName;
+            textBox1.Text += s + "\r\n";
+        }
+
+        private IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
